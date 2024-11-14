d5d4da44e2049ca44c97b198c733d25255818536
daniilseliverstov <selivrstov91@internet.ru>
2024-11-14 22:48:58 +0300

    Пагинация

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,13 +4,12 @@ from .models import Post
 
 
 def post_list(request):
-    # Получаем все посты
     posts = Post.objects.all()
 
-    # Создаем пагинатор
-    paginator = Paginator(posts, 10)  # 10 постов на странице
+    posts_per_page = request.GET.get('posts_per_page', 10)
 
-    # Преобразуем page_number в целое число
+    paginator = Paginator(posts, posts_per_page)
+
     page_number = request.GET.get('page', 1)
     page_posts = None
 
@@ -18,8 +17,6 @@ def post_list(request):
         page_number = int(page_number)
         page_posts = paginator.get_page(page_number)
     except ValueError:
-        # Если page_number не является числом, возвращаемся на первую страницу
         page_posts = paginator.page(1)
 
-    # Передаем контекст в шаблон
-    return render(request, 'post_list.html', {'page_posts': page_posts})
+    return render(request, 'post_list.html', {'page_posts': page_posts, 'posts_per_page': posts_per_page})
